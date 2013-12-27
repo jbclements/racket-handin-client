ac7572e5067ce757fa2c20cdf957221e6751c48e
John Clements <clements@racket-lang.org>
2013-12-27 15:17:26 -0800

    update for csc430-wi14

--- csc430-wi14-handin/info.rkt
+++ csc430-wi14-handin/info.rkt
@@ -5,7 +5,7 @@
 ;; Also replace the "server-cert.pem" files.
 
 ;; Your course name (used for menus, button label, collection name etc)
-(define name "CPE 123")
+(define name "CPE 430")
 
 ;; The handin tool is disabled by default, uncomment these three lines to
 ;; enable it
@@ -22,7 +22,7 @@
 ;; the specified web page (using the user's chosen web
 ;; browser.)
 (define web-menu-name "Course Homepage")
-(define web-address "http://www.brinckerhoff.org/clements/csc123-fa13/")
+(define web-address "http://www.brinckerhoff.org/clements/csc430-wi14/")
 
 ;; Auto-updater section (see handin-server/doc.txt for details)
 ;(define enable-auto-update #t) ; enable auto-update?
