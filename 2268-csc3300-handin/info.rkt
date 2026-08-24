bd90d521277c364f2008ed326e23de63fc5f099f
John Clements <clements@racket-lang.org>
2026-08-23 18:03:24 -0700

    coursenum fix

--- 2268-csc3300-handin/info.rkt
+++ 2268-csc3300-handin/info.rkt
@@ -5,7 +5,7 @@
 ;; Also replace the "server-cert.pem" files.
 
 ;; Your course name (used for menus, button label, collection name etc)
-(define name "CSC 3330")
+(define name "CSC 3300")
 
 ;; The handin tool is disabled by default, uncomment these three lines to
 ;; enable it
@@ -22,7 +22,7 @@
 ;; the specified web page (using the user's chosen web
 ;; browser.)
 (define web-menu-name "Course Homepage")
-(define web-address "http://www.brinckerhoff.org/clements/2268-csc3330/")
+(define web-address "http://www.brinckerhoff.org/clements/2268-csc3300/")
 
 ;; Auto-updater section (see handin-server/doc.txt for details)
 ;(define enable-auto-update #t) ; enable auto-update?
